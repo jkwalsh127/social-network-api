6cd6297e509c33bdd5a15c2c0592b9bf52270c6b
Jake Walsh <jkwalsh127@gmail.com>
2022-05-20 19:37:23 -0700

    initiate thought schema

---
+++ models/Thought.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            date: {
+                type: Date,
+                default: Date.now
+            },
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
