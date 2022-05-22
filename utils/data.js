b65917fdb65cb0dcade1a244a64480f0042994cb
Jake Walsh <jkwalsh127@gmail.com>
2022-05-22 11:30:20 -0700

    update seed method

--- utils/data.js
+++ utils/data.js
@@ -1,42 +1,26 @@
-const thoughts = [
-    'Apples taste good',
-    'Beets taste good',
-    'Carrots taste good',
-    'Dill taste good',
-    'Eggplant taste good',
-    'Fennel taste good',
-    'Garlic taste good',
-    'Honeydew melons taste good',
-    'Iceberg lettuce taste good',
-    'Jalapenos taste good',
-];
 
-const reaction = [
-    'Nice!',
-    'Cool!',
-    'Well said!',
-    'Agreed!',
-    'Questionable...',
-    'One way of looking at it...',
-    'Maybe reconsider this thought...',
-    'Tell us more!',
-    'Was thinking the same thing',
-    'Run for president!'
-];
 
-const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
+// const reaction = [
+//   {
+//     reactionId: '628a72b29b7e8703b41a24af',
+//     reactionBody: 'Nice!',
+//     username: otherUser,
+//     createdAt: {
+//       type: Date,
+//       default: Date.now,
+//     },
+//   },
+    // 'Cool!',
+    // 'Well said!',
+    // 'Agreed!',
+    // 'Questionable...',
+    // 'One way of looking at it...',
+    // 'Maybe reconsider this thought...',
+    // 'Tell us more!',
+    // 'Was thinking the same thing',
+    // 'Run for president!'
+// ];
 
-const getRandomThought = () =>
-  `${getRandomArrItem(thoughts)}`;
 
-const getRandomReactions = (int) => {
-    const results = [];
-    for (let i = 0; i < int; i++) {
-      results.push({
-        reactionBody: getRandomArrItem(reaction),
-      });
-    }
-    return results;
-};
   
-module.exports = { getRandomThought, getRandomReactions };
+// module.exports = { getRandomThought, getRandomReactions };
--- utils/seed.js
+++ utils/seed.js
@@ -1,6 +1,6 @@
 const connection = require('../config/connection');
 const { User, Thought } = require('../models');
-const { getRandomThought, getRandomReactions } = require('./data');
+const reactionSchema = require('../models/Reaction');
 
 connection.on('error', (err) => err);
 
@@ -13,15 +13,42 @@ connection.once('open', async () => {
   // Drop existing thoughts
   await Thought.deleteMany({});
 
-  // Create empty array to hold the thoughts
-  const thoughts = [];
+// const getRandomReactions = (int) => {
+//     const results = [];
+//     for (let i = 0; i < int; i++) {
+//       results.push({
+//         reactionBody: getRandomArrItem(reaction),
+//       });
+//     }
+//     return results;
+// };
 
-  // Get random reaction objects
-  const reactions = getRandomReactions(5);
+  // Create array to hold thoughts
+  const thought = [
+    'Beets taste good',
+    'Carrots taste good',
+    'Dill taste good',
+    'Eggplant taste good',
+    'Fennel taste good',
+    'Garlic taste good',
+    'Honeydew melons taste good',
+    'Iceberg lettuce taste good',
+    'Jalapenos taste good',
+];
+
+const thoughts = [];
+
+  const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+  const getRandomThought = () =>
+    `${getRandomArrItem(thought)}`;
+
+  // // Get random reaction objects
+  // const reactionsList = getRandomReactions(5);
 
   // Loop 10 times -- add thoughts to the thoughts array
   for (let i = 0; i < 10; i++) {
-    const thoughtText = getRandomThought();
+    const thoughtText = getRandomThought(5);
 
     thoughts.push({
       thoughtText,
@@ -33,11 +60,22 @@ connection.once('open', async () => {
   await User.collection.insertOne({
     username: 'jkwalsh',
     email: 'jkwalsh127@gmail.com',
-    thoughts: [...thoughts],
+    thought: [
+      {
+        thoughtText: 'Apples taste good',
+        createdAt: "2022-05-21T14:29:01.073Z",
+        reactions: {
+          reactionId: '628a72b29b7e8703b41a24af',
+          createdAt: "2022-05-21T14:50:40.211Z",
+          _id: "62886b209e6d1b87a826de06",
+          reactionBody: 'Nice!',
+          username: 'otherUser',
+        },
+      }
+    ]
   });
 
   console.table(thoughts);
-  console.table(reactions);
   console.info('Seeding complete! 🌱');
   process.exit(0);
 });
