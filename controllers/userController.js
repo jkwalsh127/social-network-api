02414410876bf27eca23d3b9345583d1399a2a77
Jake Walsh <jkwalsh127@gmail.com>
2022-05-22 10:51:49 -0700

    initiate friendCount

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,20 +10,34 @@ module.exports = {
   // Get all users
   getUsers(req, res) {
     User.find()
-      .then((users) => res.json(users))
-      .catch((err) => res.status(500).json(err));
+      .then(async (users) => {
+        const userObj = {
+          users,
+          friendCount: await friendCount(),
+        };
+        return res.json(userObj);
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
   },
 
   // Get a user
   getSingleUser(req, res) {
     User.findOne({ _id: req.params.userId })
     //   .select()
-      .then((user) =>
-        !user
-          ? res.status(404).json({ message: 'No user found with that ID' })
-          : res.json(user)
-      )
-      .catch((err) => res.status(500).json(err));
+      .then(async (user) => {
+        const userObj = {
+          user,
+          friendCount: await friendCount(),
+        };
+        return res.json(userObj);
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
   },
 
   // Create a user
