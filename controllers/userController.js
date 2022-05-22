949c98684a9b28846c9ca2220dfb41d7b84b4df9
Jake Walsh <jkwalsh127@gmail.com>
2022-05-22 10:43:05 -0700

    fix error in adding friend to user

--- controllers/userController.js
+++ controllers/userController.js
@@ -67,7 +67,7 @@ module.exports = {
   addFriend(req, res) {
     User.findOneAndUpdate(
       { _id: req.params.userId },
-      { $addToSet: { friends: req.body } },
+      { $addToSet: { friend: req.body } },
       { runValidators: true, new: true }
     )
       .then((user) =>
